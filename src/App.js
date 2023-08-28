2755ac8716c8d5997fa209e235d8e26b11152631
Kenny Chen <kennygchen@yahoo.com>
2023-08-27 19:38:48 -0700

    file uploader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import "./App.css";
 import { Graphviz } from "graphviz-react";
+import { FileUploader } from "./FileUploader";
+import { useState } from "react";
+
+const GraphvizOptions = {
+  // height: Math.floor(innerHeight * 0.7),
+  // width: Math.floor(innerWidth * 0.75),
+  scale: 1,
+  tweenPrecision: 1,
+  engine: "dot",
+  keyMode: "title",
+  convertEqualSidedPolygons: false,
+  fade: true,
+  growEnteringEdges: false,
+  fit: false,
+  tweenPaths: false,
+  tweenShapes: false,
+  useWorker: false,
+  zoom: true,
+};
 
 function Output() {
-  const dot = "digraph {a -> b}";
-
+  var dot = "digraph { a -> b;c; d -> c; a -> d; }";
   return (
     <div className="App-child">
       <div>Graph Visualization</div>
-      <Graphviz dot={`digraph {a -> b; c; d -> c; a -> d;}`} />
+      <Graphviz className="graph" options={GraphvizOptions} dot={dot} />
     </div>
   );
 }
 
 function Input() {
+  const [fileName, setFileName] = useState("");
+  const handleFile = (file) => {
+    const reader = new FileReader();
+    reader.onload = async (file) => {
+      const text = file.target.result;
+      console.log(text);
+      // alert(text);
+    };
+    reader.readAsText(file);
+    setFileName(file.name);
+  };
+
   return (
-    <form className="App-child">
+    <div className="App-child">
       <div>Graphviz Definition</div>
-      <textarea></textarea>
-      <button>Submit</button>
-    </form>
+      <FileUploader handleFile={handleFile} />
+      {fileName ? <p>Uploaded file: {fileName}</p> : null}
+    </div>
   );
 }
 
---
+++ src/FileUploader.js
@@ -0,0 +1,28 @@
+import { useRef } from "react";
+import "./App.css";
+
+export const FileUploader = ({ handleFile }) => {
+  const hiddenFileInput = useRef(null);
+
+  const handleClick = (event) => {
+    hiddenFileInput.current.click();
+  };
+
+  const handleChange = (event) => {
+    const fileUploaded = event.target.files[0];
+    handleFile(fileUploaded);
+  };
+  return (
+    <>
+      <button className="button-upload" onClick={handleClick}>
+        Choose a DOT file
+      </button>
+      <input
+        type="file"
+        onChange={handleChange}
+        ref={hiddenFileInput}
+        style={{ display: "none" }} // Make the file input element invisible
+      />
+    </>
+  );
+};
