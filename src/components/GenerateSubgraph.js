1a057a6f5acf40297d02313b5137d43c8334f2db
Kenny Chen <kennygchen@yahoo.com>
2023-09-19 13:48:12 -0700

    fix undefined children error

--- src/components/GenerateSubgraph.js
+++ src/components/GenerateSubgraph.js
@@ -3,10 +3,46 @@ export default function GenerateSubgraph(node) {
     nodes: [],
     links: [],
   };
-  const neighborHalf = node.neighbors.length / 2;
-  const linksHalf = node.links.length / 2;
-  subgraph.nodes.push(...node.neighbors.slice(0, neighborHalf));
-  subgraph.links.push(...node.links.slice(0, linksHalf));
-  subgraph.nodes.push(node);
+  const parentNode = {
+    key: node.key,
+    color: node.color,
+    attributes: {
+      label: node.attributes.label,
+      modularity_class: node.attributes.modularity_class,
+      MemoryObject: node.attributes.MemoryObject,
+      Offset: node.attributes.Offset,
+    },
+  };
+  subgraph.nodes.push(parentNode);
+
+  const neighborsSet = new Set(node.neighbors);
+  const linksSet = new Set(node.links);
+
+  for (const neighbor of neighborsSet) {
+    console.log(neighbor);
+    const nodeCopy = {
+      key: neighbor.key,
+      color: neighbor.color,
+      attributes: {
+        label: neighbor.attributes.label,
+        modularity_class: neighbor.attributes.modularity_class,
+        MemoryObject: neighbor.attributes.MemoryObject,
+        Offset: neighbor.attributes.Offset,
+      },
+    };
+    subgraph.nodes.push(nodeCopy);
+  }
+
+  for (const link of linksSet) {
+    const linkCopy = {
+      source: link.source.key,
+      target: link.target.key,
+      color: link.color,
+      attributes: {
+        weight: link.attributes.weight,
+      },
+    };
+    subgraph.links.push(linkCopy);
+  }
   return subgraph;
 }
